be18658a140344990b76ce64cfa217053382617f
cata <rattocatalina@gmail.com>
2024-04-25 15:05:59 -0300

    mas funciones y anotaciones

--- Clase15-Asincronia y promesas/js/main.js
+++ Clase15-Asincronia y promesas/js/main.js
@@ -1,3 +1,26 @@
-console.log('priemro');
-console.log('segundo');
-console.log('tercero');
+// ! setTimeOut
+// console.log('priemro');
+// setTimeout(() => {
+//   console.log("mitad")
+// }, 3000); //aparece al final porqe estamos esperando 3 secs 
+
+// console.log('tercero');
+
+//! setInterval 
+let contador = 0 ; 
+let parrafoContador = document.querySelector("#contador");
+
+
+const segundos = setInterval(() => {
+  contador++; 
+  parrafoContador.innerText = contador;
+
+  if(contador===10){
+    clearInterval(segundos)
+    console.log("Borramos el intervalo")
+  }
+}, 1000); //para hacer contadores 
+
+
+
+
